e8ea3aa10ff1b0ddb0df6143009a886da12cddb9
VladimirSiTozi <132054352+VladimirSiTozi@users.noreply.github.com>
2025-03-09 11:22:27 +0200

    fix: HomePage > add: home-product-card html

--- TrainerAppIvan_BackEnd2/common/views.py
+++ TrainerAppIvan_BackEnd2/common/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 
+from TrainerAppIvan_BackEnd2.product.models import Product
+
 
 class HomePageView(TemplateView):
     template_name = 'common/home.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['products'] = Product.objects.filter(is_active=True)
+        return context
 
 
 class CoachingPageView(TemplateView):

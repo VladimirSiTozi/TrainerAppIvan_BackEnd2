e339c4d9d5c55801ece652e6a1e602c2870b6192
VladimirSiTozi <132054352+VladimirSiTozi@users.noreply.github.com>
2025-07-19 17:39:12 +0300

    fix: exercise template model

--- TrainerAppIvan_BackEnd2/program/models.py
+++ TrainerAppIvan_BackEnd2/program/models.py
@@ -45,7 +45,7 @@ class ExerciseTemplate(models.Model):
     description = models.TextField(blank=True, null=True)
     focus = models.CharField(max_length=100, blank=True, null=True)  # e.g., "Chest"
     youtube_video_id = models.CharField(max_length=20, blank=True, null=True)
-    image = models.ImageField(blank=True, null=True)
+    image = models.ImageField(upload_to='exercises/images/', blank=True, null=True)
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
